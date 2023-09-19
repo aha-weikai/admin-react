2c7887f4061a2c6ee1811d0835249e7843559dd1
weikai <weikai999828@gmail.com>
2023-09-19 23:24:35 +0800

    chore: 校验账号和密码

--- src/utils/regexp.ts
+++ src/utils/regexp.ts
@@ -1,5 +1,12 @@
 export function verifyPassword(password: string) {
+  if (typeof password !== "string") throw new TypeError("Expected a string");
   const reg =
     /^(?![a-zA-Z]+$)(?![A-Z0-9]+$)(?![A-Z\W_!@#$%^&*`~()-+=]+$)(?![a-z0-9]+$)(?![a-z\W_!@#$%^&*`~()-+=]+$)(?![0-9\W_!@#$%^&*`~()-+=]+$)[a-zA-Z0-9\W_!@#$%^&*`~()-+=]/;
   return reg.test(password);
+}
+
+export function verifyAccount(str: string) {
+  if (typeof str !== "string") throw new TypeError(`Expected a string`);
+  const reg = /^[\w-]{4,16}$/;
+  return reg.test(str);
 }

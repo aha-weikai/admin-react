129f09f45801faa1323b4bc0435397230492d6bf
kaixin <1900461953@qq.com>
2023-09-26 17:34:38 +0800

    chore: 抽出plainToInstance

---
+++ src/utils/classTransformer.ts
@@ -0,0 +1,13 @@
+import { ClassConstructor, ClassTransformOptions, plainToInstance as _plainToInstance } from "class-transformer";
+
+/**
+ * # 通过类，去除对象中多余的属性
+ * Converts plain (literal) object to class (constructor) object. Also works with arrays.
+ * @param cls
+ * @param plain
+ * @param options
+ */
+export function plainToInstance<T, V>(cls: ClassConstructor<T>, plain: V, options?: ClassTransformOptions | undefined): T;
+export function plainToInstance<T, V>(cls: ClassConstructor<T>, plain: V[], options?: ClassTransformOptions | undefined): T[] {
+  return _plainToInstance(cls, plain, { excludeExtraneousValues: true, ...options });
+}
